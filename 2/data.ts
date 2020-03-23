c7f9efcff3738f4d75bf40983f97ad7760110cf6
Pedro de León <38962557+Turtugilla@users.noreply.github.com>
2020-03-23 09:54:29 -0600

    Actualización Estructura
    mejorar la organización de las carpetsa y agrege los archivos data.ts y model.ts

---
+++ 2/data.ts
@@ -0,0 +1,40 @@
+import { Automovil } from './models';
+
+export const AUTOMOVILES: Automovil[] = [
+    {
+        marca: 'Toyota',
+        submarca: 'Corolla',
+        modelo: 2010,
+        descripcion: 'Toyota Corolla 4 Puertas Estandar',
+        fecha_registro: new Date('2020-03-10')
+    },
+    {
+        marca: 'Honda',
+        submarca: 'Civic',
+        modelo: 2018,
+        descripcion: 'Honda Civic 4 Puertas Automático',
+        fecha_registro: new Date('2020-03-10')
+    },
+    {
+        marca: 'Honda',
+        submarca: 'Civic',
+        modelo: 2018,
+        descripcion: 'Honda Civic 4 Puertas Estandar',
+        fecha_registro: new Date('2020-03-10')
+    },
+    {
+        marca: 'Toyota',
+        submarca: 'Camry',
+        modelo: 2010,
+        descripcion: 'Toyota Camry 4 Puertas Estandar',
+        fecha_registro: new Date('2020-03-15')
+    },
+    {
+        marca: 'Honda',
+        submarca: 'City',
+        modelo: 2010,
+        descripcion: 'Honda City 5 Puertas Automático Asientos Piel',
+        fecha_registro: new Date('2020-03-15')
+    }
+
+]
---
+++ 2/models.ts
@@ -0,0 +1,7 @@
+export interface Automovil {
+    marca: string;
+    submarca: string;
+    modelo: number;
+    descripcion: string;
+    fecha_registro: Date;
+}
